775cf22687e01c06edc7df00ff1d1e4847f20889
Hobbestigrou <hobbestigrou@erakis.im>
2012-05-12 20:53:38 +0200

    [Core] Sslify: Add documentation.

--- lib/Dancer/Plugin/SSLify.pm
+++ lib/Dancer/Plugin/SSLify.pm
@@ -8,6 +8,17 @@ use Dancer::Plugin;
 
 #ABSTRACT: Configure your application to redirect all incoming requests to HHTPS
 
+=method sslify
+
+    sslify();
+
+Redirect all incoming requests to https.
+
+    input: none
+    output: none
+
+=cut
+
 register sslify => sub {
     hook before => sub {
         my $req = request;
@@ -48,3 +59,49 @@ sub _set_hsts_header {
 register_plugin;
 
 1;
+
+=encoding UTF-8
+
+=head1 SYNOPSIS
+
+    use Dancer ':syntax';
+    use Dancer::Plugin::SSLify;
+
+    sslify();
+
+    get '/' => sub {
+        template index;
+    }
+
+=head1 CONFIGURATION
+
+  plugins:
+    SSLify:
+      hsts_age: 31536000
+      hsts_include_subdomains: 0
+
+=head1 CONTRIBUTING
+
+This module is developed on Github at:
+
+L<http://github.com/hobbestigrou/Dancer-Plugin-SSLify>
+
+=head1 ACKNOWLEDGEMENTS
+
+Inspired by flask-sslify developed by Kenneth Reitz
+
+=head1 BUGS
+
+Please report any bugs or feature requests in github.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Dancer::Plugin::SSLify
+
+=head1 SEE ALSO
+
+L<Dancer>
+
+=cut
